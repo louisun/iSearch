16ad072f9663f9935534c654db366c0fe607dc41
louisun <read_on@126.com>
2017-03-18 21:11:59 +0800

    support python2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
